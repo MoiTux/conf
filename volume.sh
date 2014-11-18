27d17d202f3b891eb8700b91e8aca9674b0f8ba6
MoiTux <moitux@gandi.net>
2014-11-18 01:24:41 +0100

    show the current level of the volume

--- volume.sh
+++ volume.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # use PulseAudio to easly set the volume up/down/mute
+# also use notify-send (libnotify-bin) to notif the level's volume
 
 if [ $# -ne 1 ];
 then
@@ -12,4 +13,17 @@ then
     pactl set-sink-mute "${device}" toggle
 else
     pactl set-sink-volume "${device}" -- "$1"
+fi
+id=$(pactl list short sinks | grep "${device}" | cut -f 1)
+id=$(( $id + 1 ))
+volume=$(pactl list sinks | grep 'Mute' | sed -n "${id}p" | cut -d ' ' -f 2)
+if [ "${volume}" = 'no' ];
+then
+    volume=$(pactl list sinks | grep -E '[^ ]Volume:'  | sed -n "${id}p" | \
+                    cut -d / -f 2)
+    echo 'Volume is at:' $volume
+    notify-send -t 1 'Volume is at: ' "${volume}"
+else
+    echo 'Volume: Mute'
+    notify-send -t 1 'Volume: ' 'Mute'
 fi
